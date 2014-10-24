675911e63ef3a86f4062854f994058bf9328d915
pablo <pablo@gmail.com>
2014-10-24 03:11:21 -0600

    Imagenes

--- src/main/java/main/Commons.java
+++ src/main/java/main/Commons.java
@@ -13,7 +13,15 @@ public interface Commons {
     public static final String NOT = "Compuerta NOT";
     public static final String XOR = "Compuerta XOR";
     public static final String XNOR = "Compuerta XNOR";
-    public static final String XML = "Compuerta desde XML";
+    public static final String CUSTOM = "Componente Externo";
+    public static final String AND_PATH = "src/res/image-01.png";
+    public static final String NAND_PATH = "src/res/image-06.png";
+    public static final String OR_PATH = "src/res/image-03.png";
+    public static final String NOR_PATH = "src/res/image-07.png";
+    public static final String NOT_PATH = "src/res/image-02.png";
+    public static final String XOR_PATH = "src/res/image-04.png";
+    public static final String XNOR_PATH = "src/res/image-05.png";
+    public static final String CUSTOM_PATH = "src/res/image-08.png";
 }
 
 
--- src/main/java/model/CompuertaCustom.java
+++ src/main/java/model/CompuertaCustom.java
@@ -4,14 +4,14 @@ package model;
  * Compuerta que se comporta como una caja negra dependiendo de un xml
  * Created by pablo on 15/10/14.
  */
-public class CompuertaXml extends CompuertaLogica {
+public class CompuertaCustom extends CompuertaLogica {
     /**
      * Asigna la cantidad de entradas y salidas
      *
      * @param pEntradas
      * @param pSalidas
      */
-    public CompuertaXml(int pEntradas, int pSalidas) {
+    public CompuertaCustom(int pEntradas, int pSalidas) {
         super(pEntradas, pSalidas);
     }
 
