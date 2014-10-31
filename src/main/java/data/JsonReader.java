0cdf75b70992269b5253eb203efd6e02a11b1ce6
pablo <pablo@gmail.com>
2014-10-31 07:53:10 -0600

    json dependency

--- src/main/java/data/JsonReader.java
+++ src/main/java/data/JsonReader.java
@@ -1,10 +1,13 @@
 package data;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Created by pablo on 15/10/14.
  */
 public class JsonReader {
-    public JsonReader(String fileName) {
-
+    public JsonReader(String fileName) throws MalformedURLException {
+        URL url = new URL(fileName);
     }
 }
---
+++ src/main/java/data/JsonWriter.java
@@ -0,0 +1,7 @@
+package data;
+
+/**
+ * Created by pablo on 31/10/14.
+ */
+public class JsonWriter {
+}
